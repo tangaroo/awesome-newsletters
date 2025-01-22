6ca0bec12566f9e048473a79921a17a13a259838
Cassandra Tang <cassandra.tang@hotmail.com>
2025-01-22 21:55:05 +0000

    updated footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -5,7 +5,7 @@ const FooterComponent = () => {
   return (
     <Footer>
       <p>
-        &copy; 2024 / Say hey @{" "}
+        &copy; 2025 / Say hey @{" "}
         <a href="https://cassandratang.com">cassandratang.com</a>
       </p>
     </Footer>
